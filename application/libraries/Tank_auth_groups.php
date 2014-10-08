ed60785d894360d62af938154106f01f6ed1b58a
John Wright <wrightlabs@gmail.com>
2014-10-07 21:37:49 -0700

    fix: this should be the mose appropriate method to use
    get_user_by_login

--- application/libraries/Tank_auth_groups.php
+++ application/libraries/Tank_auth_groups.php
@@ -55,7 +55,7 @@ class Tank_auth_groups extends Tank_auth {
 
 		if($loggedIn) 
 		{
-			$user = $this->ci->users->get_user_by_id($this->get_user_id());
+			$user = $this->ci->users->get_user_by_login($this->get_username());
 			$this->ci->session->set_userdata(array('group_id'	=> $user->group_id));
 		}
 				
